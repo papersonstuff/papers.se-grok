09da5497975dead2ad3e3b15d8bee894edaa58b9
papersonstuff <info@papers.se>
2025-07-19 13:36:11 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-import './globals.css';  // If you have global CSS - optional for now
-
 export const metadata = {
   title: 'Papers.se - AI News Aggregator',
   description: 'Your AI news aggregator',
