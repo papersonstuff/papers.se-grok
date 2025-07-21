da0099db0ee61a86b35768920ffbd39d36872012
papersonstuff <info@papers.se>
2025-07-21 23:17:40 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: 'Papers.se - AI News Aggregator',
-  description: 'Your AI news aggregator',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
